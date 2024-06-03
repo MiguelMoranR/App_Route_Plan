63c3b6a6734fdf1ac84f5942a3ed23734e6244f7
Samsung_Pc <miguel381@gmail.com>
2024-06-02 22:35:52 -0500

    Actualización1

--- app/src/main/java/uan/edu/co/app_route_plan/View/ui/activities/MainActivity.kt
+++ app/src/main/java/uan/edu/co/app_route_plan/View/ui/activities/MainActivity.kt
@@ -1,14 +1,19 @@
 package uan.edu.co.app_route_plan.View.ui.activities
 
 import android.os.Bundle
+import android.os.Handler
 import androidx.appcompat.app.AppCompatActivity
-import uan.edu.co.app_route_plan.R
+import uan.edu.co.app_route_plan.databinding.ActivitymainBinding
 
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivitymainBinding
+    lateinit var handler: Handler
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activitymain)
+        binding= ActivitymainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
     }
 
 }
